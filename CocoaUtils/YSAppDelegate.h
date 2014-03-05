fc41902c4d8a20693cdea54c40f3f5895b416b2b
Yuan Zheng <yuanzheng@sxzw.tv>
2014-03-05 16:28:31 +0800

    Initial Commit

---
+++ CocoaUtils/YSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YSAppDelegate.h
+//  CocoaUtils
+//
+//  Created by sxzw on 14-3-5.
+//  Copyright (c) 2014年 YuenSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CocoaUtils/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CocoaUtils
+//
+//  Created by sxzw on 14-3-5.
+//  Copyright (c) 2014年 YuenSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YSAppDelegate class]));
+    }
+}
